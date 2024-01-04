1fed80654c2cc3bafb710f25c2b2f102d5541c9d
Alexmgm3d <87781716+Alexmgm3d@users.noreply.github.com>
2024-01-04 08:59:22 -0300

    Complemento JumP
    Complemento do pulo
    adaptação de salto e velocidade.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,23 +7,55 @@ public class Player : MonoBehaviour
 
 
     public Rigidbody2D myrigidbody2d;
-    public Vector2 velocity;
+    //public Vector2 velocity;
+    public Vector2 friction = new Vector2(.1f, 0);
     public float speed;
+    public float forcejump = 2;
+   
 
     private void Update()
     {
-    
-       
-        if(Input.GetKey(KeyCode.RightArrow))
+        Handlejump();
+        Handlemoviment();
+        
+    }
+
+
+        private void Handlemoviment()
         {
-            //Movimentação do personagem - myrigidbody2d.MovePosition(myrigidbody2d.position + velocity * Time.deltaTime);
-            myrigidbody2d.velocity = new Vector2(speed, myrigidbody2d.velocity.y);
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                //Movimentação do personagem - myrigidbody2d.MovePosition(myrigidbody2d.position + velocity * Time.deltaTime);
+                myrigidbody2d.velocity = new Vector2(speed, myrigidbody2d.velocity.y);
+            }
+
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                // movimentação do personagem - myrigidbody2d.MovePosition(myrigidbody2d.position - velocity * Time.deltaTime);
+                myrigidbody2d.velocity = new Vector2(-speed, myrigidbody2d.velocity.y);
+
+            }
+
+            if (myrigidbody2d.velocity.x > 0)
+        {
+                myrigidbody2d.velocity += friction;
         }
 
-        else if(Input.GetKey(KeyCode.LeftArrow))
+            else if (myrigidbody2d.velocity.x <0)
         {
-            // movimentação do personagem - myrigidbody2d.MovePosition(myrigidbody2d.position - velocity * Time.deltaTime);
-            myrigidbody2d.velocity = new Vector2(-speed, myrigidbody2d.velocity.y);
+            myrigidbody2d.velocity -= friction;
         }
+
+    }
+
+    private void Handlejump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            myrigidbody2d.velocity = Vector2.up * forcejump; 
+        }
+
     }
 }
+
+
