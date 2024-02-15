c6e10adbabf489982dbf61b23ebe7cefc624daed
Alexmgm3d <87781716+Alexmgm3d@users.noreply.github.com>
2024-02-15 09:27:30 -0300

    Correção Animação
    Correção de Animação do pulo

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,10 +5,8 @@ using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
-
-
     public Rigidbody2D myrigidbody2d;
-    //public Vector2 velocity;
+
     [Header("Speed Setup")]
     public Vector2 friction = new Vector2(.1f, 0);
     public float speed;
@@ -21,106 +19,80 @@ public class Player : MonoBehaviour
     public float animationDuration = .3f;
     public Ease ease = Ease.OutBack;
 
-
     [Header("Animation Player")]
     public string boolRun = "Run";
     public Animator animator;
     public float playerSwipeDuration = .1f;
 
-
-
     private float _currentSpeed;
+    private Vector3 originalScale;
 
+    private void Start()
+    {
+        originalScale = transform.localScale;
+    }
 
     private void Update()
     {
-        Handlejump();
-        Handlemoviment();
-
+        HandleJump();
+        HandleMovement();
     }
 
-
-    private void Handlemoviment()
+    private void HandleMovement()
     {
         if (Input.GetKey(KeyCode.LeftShift))
         {
             _currentSpeed = speedRun;
             animator.speed = 2;
         }
-
-
         else
-        { 
+        {
             _currentSpeed = speed;
             animator.speed = 1;
         }
 
-
-        if (Input.GetKey(KeyCode.RightArrow))
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        if (horizontalInput != 0)
         {
-            //Movimentação do personagem - myrigidbody2d.MovePosition(myrigidbody2d.position + velocity * Time.deltaTime);
-            myrigidbody2d.velocity = new Vector2(_currentSpeed, myrigidbody2d.velocity.y);
-            if(myrigidbody2d.transform.localScale.x != 1)
-            {
-                myrigidbody2d.transform.DOScaleX(1, playerSwipeDuration);
-
-            }
-
             animator.SetBool(boolRun, true);
-        }
+            myrigidbody2d.velocity = new Vector2(horizontalInput * _currentSpeed, myrigidbody2d.velocity.y);
 
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            // movimentação do personagem - myrigidbody2d.MovePosition(myrigidbody2d.position - velocity * Time.deltaTime);
-            myrigidbody2d.velocity = new Vector2(-_currentSpeed, myrigidbody2d.velocity.y);
-            if (myrigidbody2d.transform.localScale.x != -1)
+            if (myrigidbody2d.velocity.x > 0 && transform.localScale.x < 0)
             {
-                myrigidbody2d.transform.DOScaleX(-1, playerSwipeDuration);
-
+                Flip();
             }
-            animator.SetBool(boolRun, true);
-
-
+            else if (myrigidbody2d.velocity.x < 0 && transform.localScale.x > 0)
+            {
+                Flip();
+            }
         }
-
         else
         {
-
             animator.SetBool(boolRun, false);
         }
-
 
         if (myrigidbody2d.velocity.x > 0)
         {
             myrigidbody2d.velocity += friction;
         }
-
         else if (myrigidbody2d.velocity.x < 0)
         {
             myrigidbody2d.velocity -= friction;
         }
-
     }
 
-    private void Handlejump()
+    private void HandleJump()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             myrigidbody2d.velocity = Vector2.up * forcejump;
-            myrigidbody2d.transform.localScale = Vector2.one;
-            DOTween.Kill(myrigidbody2d.transform);
-
-            HandleScaleJump();
-
+            DOTween.Kill(transform);
+            // Não faz nada com a escala do transform durante o pulo
         }
     }
 
-
-
-    private void HandleScaleJump()
+    private void Flip()
     {
-        myrigidbody2d.transform.DOScaleY(jumpScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
-        myrigidbody2d.transform.DOScaleX(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
     }
-
 }
